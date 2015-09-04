bb5f1c90ecec1180a66456aa1708d06a29377c25
Chris Lemper <clemper@sovrn.com>
2015-09-04 09:26:39 -0600

    Update copyright and use fill name in author tag.

--- src/main/java/org/springframework/integration/kafka/listener/KafkaNativeOffsetManager.java
+++ src/main/java/org/springframework/integration/kafka/listener/KafkaNativeOffsetManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ import kafka.utils.ZKStringSerializer$;
 /**
  * Implementation of an {@link OffsetManager} that uses kafka native 'topic' offset storage.
  *
- * @author clemper
+ * @author Chris Lemper
  */
 public class KafkaNativeOffsetManager extends AbstractOffsetManager implements InitializingBean {
 
